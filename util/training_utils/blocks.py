d4f3e36ad0b032877566144f12ca8052fabd6fcf
Adam <adamhung@andrew.cmu.edu>
2026-02-16 13:21:00 -0500

    rm some unused pytorch code

--- util/training_utils/blocks.py
+++ util/training_utils/blocks.py
@@ -109,23 +109,6 @@ class Attention(nn.Module):
         x = self.proj_drop(x)
         return x
 
-class Block(nn.Module):
-
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0.,
-                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
-        super().__init__()
-        self.norm1 = norm_layer(dim)
-        self.attn = Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        self.norm2 = norm_layer(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-
-    def forward(self, x, xpos):
-        x = x + self.drop_path(self.attn(self.norm1(x), xpos))
-        x = x + self.drop_path(self.mlp(self.norm2(x)))
-        return x
-
 class CrossAttention(nn.Module):
     
     def __init__(self, dim, num_heads=8, qkv_bias=False, attn_drop=0., proj_drop=0.,use_flash_attn=True):
@@ -202,53 +185,4 @@ class DecoderBlock(nn.Module):
             if self.batch_drop_path_prob==0.0 or not self.training or torch.rand(1).item()>=self.batch_drop_path_prob: x = x + self.drop_path(self.cross_attn(self.norm2(x), y_, y_, xpos, ypos))
             if self.batch_drop_path_prob==0.0 or not self.training or torch.rand(1).item()>=self.batch_drop_path_prob: x = x + self.drop_path(self.attn(self.norm1(x), xpos))
             if self.batch_drop_path_prob==0.0 or not self.training or torch.rand(1).item()>=self.batch_drop_path_prob: x = x + self.drop_path(self.mlp(self.norm3(x)))
-        return x, y
-        
-        
-# patch embedding
-class PositionGetter(object):
-    """ return positions of patches """
-
-    def __init__(self):
-        self.cache_positions = {}
-        
-    def __call__(self, b, h, w, device):
-        if not (h,w) in self.cache_positions:
-            x = torch.arange(w, device=device)
-            y = torch.arange(h, device=device)
-            self.cache_positions[h,w] = torch.cartesian_prod(y, x) # (h, w, 2)
-        pos = self.cache_positions[h,w].view(1, h*w, 2).expand(b, -1, 2).clone()
-        return pos
-
-class PatchEmbed(nn.Module):
-    """ just adding _init_weights + position getter compared to timm.models.layers.patch_embed.PatchEmbed"""
-
-    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768, norm_layer=None, flatten=True):
-        super().__init__()
-        img_size = to_2tuple(img_size)
-        patch_size = to_2tuple(patch_size)
-        self.img_size = img_size
-        self.patch_size = patch_size
-        self.grid_size = (img_size[0] // patch_size[0], img_size[1] // patch_size[1])
-        self.num_patches = self.grid_size[0] * self.grid_size[1]
-        self.flatten = flatten
-
-        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
-        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
-        
-        self.position_getter = PositionGetter()
-        
-    def forward(self, x):
-        B, C, H, W = x.shape
-        torch._assert(H == self.img_size[0], f"Input image height ({H}) doesn't match model ({self.img_size[0]}).")
-        torch._assert(W == self.img_size[1], f"Input image width ({W}) doesn't match model ({self.img_size[1]}).")
-        x = self.proj(x)
-        pos = self.position_getter(B, x.size(2), x.size(3), x.device)
-        if self.flatten:
-            x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
-        x = self.norm(x)
-        return x, pos
-        
-    def _init_weights(self):
-        w = self.proj.weight.data
-        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1])) 
+        return x, y 
